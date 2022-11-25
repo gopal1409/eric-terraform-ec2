d82242a39ebcc7e420fb8dc7d6b4bd1a272eab1a
John <gopal1409@gmail.com>
2022-11-25 16:55:25 +0530

    terraform ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.awzlinux2.id
   instance_type = var.instance_type
-  user_data = file("${path.module/app1-install.sh}")
+  user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
   tags = {
     Name = "ec2-demo"
